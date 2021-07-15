4c8aaf5c2ac15777b82978424f2b6d0144bea267
manhtran1611 <85244387+manhtran1611@users.noreply.github.com>
2021-07-16 00:47:07 +0700

    Edit TodoApp

---
+++ src/TodoList.js
@@ -0,0 +1,26 @@
+import React from "react";
+import Paper from "@material-ui/core/Paper";
+import List from "@material-ui/core/List";
+import Divider from "@material-ui/core/Divider";
+import Todo from "./Todo";
+
+export default function TodoList({ todos, removeTodo, toggleTodo, editTodo }) {
+  return (
+    <Paper>
+      <List>
+        {todos.map((todo, i) => (
+          <>
+            <Todo
+              {...todo}
+              key={todo.id}
+              removeTodo={removeTodo}
+              toggleTodo={toggleTodo}
+              editTodo={editTodo}
+            />
+            {i < todos.length - 1 && <Divider />}
+          </>
+        ))}
+      </List>
+    </Paper>
+  );
+}
